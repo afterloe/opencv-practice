e7cfc4c9cc8e6dbe59b98aaf7a91d313cb02da0f
afterloe <605728727@qq.com>
2019-11-24 17:06:34 +0800

    feat: 自动canny边缘检测

--- 6-learn.py
+++ 6-learn.py
@@ -7,10 +7,8 @@ img = cv2.imread("../tmp/3.png", cv2.IMREAD_COLOR)
 cv2.imshow("before", img)
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-cv2.imshow("gray", gray)
-# 骨架化, size 为 结构元素内核大小
-skeleton = imutils.skeletonize(gray, size=(3, 3))
-cv2.imshow("after", skeleton)
+edgeMap = imutils.auto_canny(gray)
+cv2.imshow("Automatic Edge Map", edgeMap)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
