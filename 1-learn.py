48b17e0e35e3c9cd1f0724e8d36196171825799c
afterloe <605728727@qq.com>
2019-11-24 13:55:43 +0800

    feat: python3 的一些特殊性

--- 1-learn.py
+++ 1-learn.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python3
 
 print("hello 江南小炮王")
+
+x = "a"
+y = "b"
+
+'''
+ 不换行输出
+'''
+print(x, end=" ")
+print(y, end=" ")
+print()
+
+str = "Runoob"
+print(str[0:-1])
+print(str[2:5])
