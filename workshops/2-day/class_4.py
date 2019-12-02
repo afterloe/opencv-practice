9d161b21b71ae761d1876153b2da9ed222e3f106
afterloe <lm6289511@gmail.com>
2019-12-02 15:16:14 +0800

    feat: 图像插值的运用

---
+++ workshops/2-day/class_4.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding=utf-8 -*-
+
+"""
+    图像插值：用于几何变换（图像放大、缩小等操作）、透视变换、插值计算新像素。
+    常用的算法为以下四种：
+        临近点插值算法 - 速度快
+        双线性插值
+        lanczos插值 - 提升精准度
+        双立方插值
+
+    清晰度逐渐提升，
+"""
+import cv2 as cv
+
+src = cv.imread("../../pic/rmb/1.png", cv.IMREAD_COLOR)
+h, w = src.shape[:2]  # 从0 开始，取两位; shape[h, w, ch]
+
+print("image is %d * %d" % (h, w))
+
+dst = cv.resize(src, (w * 2, h * 2), interpolation=cv.INTER_NEAREST)  # 临近点算法
+cv.imshow("NEAREST", dst)
+
+dst = cv.resize(src, (w * 2, h * 2), interpolation=cv.INTER_LINEAR)  # 双线性插值算法
+cv.imshow("LINEAR", dst)
+
+dst = cv.resize(src, (w * 2, h * 2), interpolation=cv.INTER_LANCZOS4)  # lanczos4插值算法
+cv.imshow("LANCZOS4", dst)
+
+dst = cv.resize(src, (w * 2, h * 2), interpolation=cv.INTER_CUBIC)  # 双立方插值算法
+cv.imshow("CUBIC", dst)
+
+cv.waitKey(0)
+cv.destroyAllWindows()
