3e60bc85e3e31ec8c68b047de5cad00311303c58
afterloe <lm6289511@gmail.com>
2019-12-02 17:37:49 +0800

    feat: 职责链重构

--- workshops/2-day/class_5.py
+++ workshops/2-day/class_5.py
@@ -10,6 +10,46 @@ WIDTH = 512
 THICKNESS = 1
 
 
+class DrawChain(object):
+    def __init__(self):
+        self.successor = ''
+
+    def set_successor(self, successor):
+        self.successor = successor
+        return successor
+
+    def handle_request(self, command, canvas):
+        pass
+
+
+class DrawChainRectangle(DrawChain):
+    def handle_request(self, command, canvas):
+        if 1 != command['shape'] and 0 != command['shape']:
+            return self.successor.handle_request(command, canvas)
+        thickness = cv.FILLED if command['fill'] else THICKNESS
+        cv.rectangle(canvas, generatorPointer(), generatorPointer(), generatorColor(), thickness, cv.LINE_8)
+        return canvas
+
+
+class DrawChainCircle(DrawChain):
+    def handle_request(self, command, canvas):
+        if 2 != command['shape'] and 0 != command['shape']:
+            return self.successor.handle_request(command, canvas)
+        thickness = cv.FILLED if command['fill'] else THICKNESS
+        r, _ = generatorPointer()
+        cv.circle(canvas, generatorPointer(), r, generatorColor(), thickness, cv.LINE_8)
+        return canvas
+
+
+class DrawChainEllipse(DrawChain):
+    def handle_request(self, command, canvas):
+        if 3 != command['shape'] and 0 != command['shape']:
+            return self.successor.handle_request(command, canvas)
+        thickness = cv.FILLED if command['fill'] else THICKNESS
+        cv.ellipse(canvas, generatorPointer(), generatorPointer(), 360, 0, 360, generatorColor(), thickness, cv.LINE_8)
+        return canvas
+
+
 def initArg():
     ap = argparse.ArgumentParser()
     ap.description = "opencv draw by afterloe version 1.0.0"
@@ -33,55 +73,8 @@ def generatorPointer():
     return np.int(x), np.int(y)
 
 
-def drawRectangle(canvas, command):
-    if 1 != command['shape'] and 0 != command['shape']:
-        return False, command, canvas
-    thickness = cv.FILLED if command['fill'] else THICKNESS
-    x, y = generatorPointer()
-    x1, y1 = generatorPointer()
-    b, g, r = generatorColor()
-    print("draw rectangle (%d, %d) rgb is (%d, %d, %d) width is %d" % (x, y, r, g, b, thickness))
-    cv.rectangle(canvas, (x, y), (x1, y1), (b, g, r), thickness, cv.LINE_8)
-    return True, command, canvas
-
-
-def drawCircle(canvas, command):
-    if 2 != command['shape'] and 0 != command['shape']:
-        return False, command, canvas
-    thickness = cv.FILLED if command['fill'] else THICKNESS
-    r, _ = generatorPointer()
-    x, y = generatorPointer()
-    b, g, r = generatorColor()
-    cv.circle(canvas, (x, y), r, (b, g, r), thickness, cv.LINE_8)
-    return True, command, canvas
-
-
-def drawEllipse(canvas, command):
-    if 3 != command['shape'] and 0 != command['shape']:
-        return False, command, canvas
-    thickness = cv.FILLED if command['fill'] else THICKNESS
-    x, y = generatorPointer()
-    x1, y1 = generatorPointer()
-    b, g, r = generatorColor()
-    cv.ellipse(canvas, (x, y), (x1, y1), 360, 0, 360, (b, g, r), thickness, cv.LINE_8)
-    return True, command, canvas
-
-
-def drawAll(canvas, command):
-    if 0 != command['shape']:
-        return False, command, canvas
-    _, _, canvas = drawRectangle(canvas, command)
-    _, _, canvas = drawCircle(canvas, command)
-    _, _, canvas = drawEllipse(canvas, command)
-    return True, command, canvas
-
-
 def drawChain(canvas, command):
-    flag, _, canvas = drawAll(canvas, command)
-    if not flag:
-        _, _, canvas = drawRectangle(canvas, command)
-        _, _, canvas = drawCircle(canvas, command)
-        _, _, canvas = drawEllipse(canvas, command)
+    canvas = chain_rectangle.handle_request(command, canvas)
     cv.imshow("image", canvas)
 
 
@@ -90,14 +83,21 @@ arg_map = initArg()
 print(arg_map)
 # 创建画布
 mat = np.zeros(shape=(512, 512, 3), dtype=np.uint8)
+
+# 初始化职责链
+chain_rectangle = DrawChainRectangle()
+chain_circle = DrawChainCircle()
+chain_ellipse = DrawChainEllipse()
+chain_rectangle.set_successor(chain_circle).set_successor(chain_ellipse)
+
 if arg_map['loop']:
     for _ in range(100000):
-        drawChain(mat, arg_map)
+        drawChain(canvas=mat, command=arg_map)
         input_key = cv.waitKey(20)
         if 27 == input_key:  # esc
             break
 else:
-    drawChain(mat, arg_map)
+    drawChain(canvas=mat, command=arg_map)
     cv.waitKey(0)
 
 cv.destroyAllWindows()
