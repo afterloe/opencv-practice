b78cfa5ad7695764219e37ef9c117aedc638659d
afterloe <605728727@qq.com>
2019-12-31 17:19:49 +0800

    refactor: 动态点添加

---
+++ test/list_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding=utf-8 -*-
+
+
+def main():
+    ap = []
+    bp = ["afterloe", "joe", "next"]
+    ap += bp
+    print(len(ap))
+    print(ap)
+
+
+if "__main__" == __name__:
+    main()
