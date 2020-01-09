3935c60418136f42e2f1719d6bcd427faeb39ba1
afterloe <605728727@qq.com>
2020-01-09 11:39:37 +0800

    docs: 添加orb方法的描述信息

--- workshops/10-section/3-clazz.py
+++ workshops/10-section/3-clazz.py
@@ -10,18 +10,31 @@ import numpy as np
     而值点对，ORB通过FAST方法，FAST方式寻找候选特征点方式是假设灰度图像像素点A周围的像素存在连续大于或者小于A的灰度值。至少其中三个点
     满足上述不等式条件，即可将P视为候选点，然后通过阈值进行最终的筛选即可得到ORB特征点。相关API描述如下：
     
-    cv.ORB::create()
+    cv.ORB::create(nfeatures, scaleFactor, nlevels, edgeThreshold, firstLevel, WTA_K, scoreType, patchSize,
+                    fastThreshold)
+        - nfeatures: 最终输出最大特征点数目
+        - scaleFactor: 金字塔上采样比率
+        - nlevels: 金字塔层数
+        - edgeThreshold: 边缘阈值
+        - firstLevel:  从第几层级开始，默认为0，即第一层
+        - WTA_K: BRIEF描述算子所用的
+        - scoreType: 对所有的特征点进行排民用的方法
+        - patchSize: 定向修复的大小，越大修复比率越高
+        - fastThreshold: fast算法阈值 
 """
+
+MAX_POINTS = 500
+orb_param = dict(nfeatures=MAX_POINTS, scaleFactor=1.2, nlevels=8, edgeThreshold=31, firstLevel=0)
+color = np.random.randint(0, 255, (MAX_POINTS, 3))
 
 
 def main():
     src = cv.imread("../../../raspberry-auto/pic/ele_panel.jpg")
     blur = cv.medianBlur(src, 3)
     cv.imshow("src", src)
-    orb = cv.ORB.create()
+    orb = cv.ORB.create(**orb_param)
     kps = orb.detect(blur)
     i = 0
-    color = np.random.randint(0, 255, (len(kps), 3))
     for kp in kps:
         x, y = kp.pt
         cv.circle(src, (np.int32(x), np.int32(y)), 5, color[i].tolist(), 2)
