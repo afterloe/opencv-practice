fec839805e62755b52f95e48b7f61799a553ba3f
afterloe <605728727@qq.com>
2020-02-05 17:19:24 +0800

    feat: HOG特征点提取

---
+++ workshops/10-section/8-clazz.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding=utf-8 -*-
+
+import cv2 as cv
+
+"""
+HOG特征匹配
+     1 -> 通过样本提取HOG描述子，生成样本特征数据
+     2 -> svm 分类学习与训练，保存为模型
+ 
+ HOG提取描述子
+     hog.compute(image, winStride, padding)
+        - image 提取图像的地址
+        - winStride HOG描述子窗口大小
+        - padding 
+"""
+
+
+def main():
+    src = cv.imread("../../../raspberry-auto/pic/Meter.jpg")
+    print(src.shape)
+    hog = cv.HOGDescriptor()
+    gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
+    fv = hog.compute(gray, winStride=(8, 8), padding=(0, 0))
+    print(len(fv))
+    print(fv)
+    cv.imshow("hog-descriptor", src)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+
+if "__main__" == __name__:
+    main()
