11d4bcf4a666692560f1c8cd221b02a8a289811e
afterloe <605728727@qq.com>
2020-02-05 21:57:48 +0800

    feat: hog+svm 识别测试

---
+++ workshops/10-section/12-clazz.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding=utf-8 -*-
+
+import cv2 as cv
+import numpy as np
+
+"""
+
+"""
+
+
+def main():
+    image = cv.imread("../../../raspberry-auto/pic/elec_watch/test/scene_08.jpg")
+    test_img = cv.resize(image, (0, 0), fx=0.2, fy=0.2)
+    cv.imshow("input", test_img)
+    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    print(test_img.shape)
+    h, w = test_img.shape[:2]
+    svm = cv.ml.SVM_load('svm_data.dat')
+    sum_x = 0
+    sum_y = 0
+    count = 0
+    hog = cv.HOGDescriptor()
+    for row in range(64, h - 64, 4):
+        for col in range(32, w - 32, 4):
+            win_roi = gray[row - 64: row + 64, col - 32: col + 32]
+            hog_desc = hog.compute(win_roi, winStride=(8, 8), padding=(0, 0))
+            one_fv = np.zeros([len(hog_desc)], dtype=np.float32)
+            for i in range(len(hog_desc)):
+                one_fv[i] = hog_desc[i][0]
+            one_fv = np.reshape(one_fv, [-1, len(hog_desc)])
+            result = svm.predict(one_fv)[1]
+            if result[0][0] > 0:
+                sum_x += (col - 32)
+                sum_y += (row - 64)
+                count += 1
+    x = sum_x // count
+    y = sum_y // count
+    cv.rectangle(test_img, (x, y), (x + 64, y + 128), (255, 0, 0), 2, cv.LINE_8, 0)
+    cv.imshow("result", test_img)
+    cv.waitKey(0)
+
+
+if "__main__" == __name__:
+    main()
+    cv.destroyAllWindows()
