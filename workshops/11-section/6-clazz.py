25cc8c2bf736d8f2de286735150748fff5217fd8
afterloe <605728727@qq.com>
2020-02-08 00:33:18 +0800

    docs: 决策树算法应用

--- workshops/11-section/6-clazz.py
+++ workshops/11-section/6-clazz.py
@@ -5,7 +5,12 @@ import cv2 as cv
 import numpy as np
 
 """
+决策树算法
+    opencv中机器学习模块的决策树算法分为两个类别，一个是随机森林，另一个强化分类，这两种算法都属于决策树算法，相关api如下
 
+cv.ml.StatModel.predict(samples, results, flags)
+    - sample 输入样本
+    - results 预测结果
 """
 
 
@@ -20,12 +25,17 @@ def main():
     train_labels = np.repeat(k, 250)[:, np.newaxis]
     test_labels = train_labels.copy()
     dt = cv.ml.RTrees_create()
+    # 训练
     dt.train(train, cv.ml.ROW_SAMPLE, train_labels)
+
+    # 预测
     _, results = dt.predict(test)
+
+    # 计算准确率
     matches = results == test_labels
     correct = np.count_nonzero(matches)
     accuracy = correct * 100.0 / results.size
-    print(accuracy)
+    print(accuracy)  # 输出准确率 83.72
 
 
 if "__main__" == __name__:
