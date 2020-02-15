4c41f62e74a3a6bc41fcee454bc428a759fce429
afterloe <605728727@qq.com>
2020-02-16 00:30:43 +0800

    feat: 手写印刷体0 ~ 9识别

--- test/match_test.py
+++ test/match_test.py
@@ -9,6 +9,12 @@ def main():
     height = pow(abs(14 - 18), 2)
     hypotenuse = math.sqrt(width + height)
     print(width, height, hypotenuse)
+    v = [0 for x in range(40)]
+    print(v)
+    v[39] = 99
+    print(v)
+    p = range(0, 40)
+    print(p)
 
 
 if "__main__" == __name__:
