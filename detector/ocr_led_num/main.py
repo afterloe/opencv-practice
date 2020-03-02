b110dc9eb86e29917519562814eb5d0b33507898
afterloe <605728727@qq.com>
2020-03-02 22:08:41 +0800

    docs: cv.Canny api内容补充

--- detector/ocr_led_num/main.py
+++ detector/ocr_led_num/main.py
@@ -2,6 +2,10 @@
 # -*- coding=utf-8 -*-
 
 import cv2 as cv
+import imutils
+from imutils import contours
+from imutils.perspective import four_point_transform
+import numpy as np
 
 """
 
@@ -9,8 +13,16 @@ import cv2 as cv
 
 
 def main():
-    pass
+    image = cv.imread("G:/Project/opencv-ascs-resources/led/2020-03-02_21-36-07.jpeg")
+    cv.imshow("input", image)
+    image = imutils.resize(image, width=500)
+    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    blurred = cv.GaussianBlur(gray, (3, 3), 0)
+    edged = cv.Canny(gray, 50, 200, 255)
+    cv.imshow("edged", edged)
+    cv.waitKey(0)
 
 
 if "__main__" == __name__:
     main()
+    cv.destroyAllWindows()
