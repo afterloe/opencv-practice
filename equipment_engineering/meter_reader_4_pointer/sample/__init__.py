39f72cda59b1f172680d286c46b3ec7e61666286
afterloe <605728727@qq.com>
2020-03-09 15:59:44 +0800

    feat: 添加测试用例

--- equipment_engineering/meter_reader_4_pointer/sample/__init__.py
+++ equipment_engineering/meter_reader_4_pointer/sample/__init__.py
@@ -1,16 +1,3 @@
 #!/usr/bin/env python3
 # -*- coding=utf-8 -*-
 
-import cv2 as cv
-
-"""
-
-"""
-
-
-def main():
-    pass
-
-
-if "__main__" == __name__:
-    main()
--- equipment_engineering/meter_reader_4_pointer/tests/test_advanced.py
+++ equipment_engineering/meter_reader_4_pointer/tests/test_advanced.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding=utf-8 -*-
 
-import cv2 as cv
-
-"""
-
-"""
+import unittest.mock
 
 
-def main():
-    pass
+class Calculator:
+    def sum(self, a, b):
+        return a + b
+
+
+class CoreTest(unittest.TestCase):
+
+    def setUp(self):
+        self.calc = Calculator()
+
+    def test_f2(self):
+        answer = self.calc.sum(2, 4)
+        self.assertEqual(answer, 6)
 
 
 if "__main__" == __name__:
-    main()
+    unittest.main()
