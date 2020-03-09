5a041436d73ce3e424db292f10414f69f1529651
afterloe <605728727@qq.com>
2020-03-09 22:03:19 +0800

    feat: 单元测试

--- equipment_engineering/meter_reader_4_pointer/tests/test_basic.py
+++ equipment_engineering/meter_reader_4_pointer/tests/test_basic.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding=utf-8 -*-
 
-import cv2 as cv
+import yaml
 
-"""
+# caps = {
+#     "min_angle": 40,
+#     "max_angle": 320,
+#     "min_value": 0,
+#     "max_value": 220
+# }
 
-"""
+with open("./config", "r", encoding="utf-8") as f:
+    # yaml.dump(caps, f)
+    value = yaml.load(f.read(), Loader=yaml.FullLoader)
+    print(value)
+    print(value["min_angle"])
 
-
-def main():
-    pass
-
-
-if "__main__" == __name__:
-    main()
