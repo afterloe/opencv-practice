36e3910d67fa8c233d312635a589c93933e923cd
afterloe <605728727@qq.com>
2020-03-09 11:12:53 +0800

    feat: gui配置

---
+++ gui/text_edit.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding=utf-8 -*-
+
+from tkinter import *
+from tkinter.scrolledtext import ScrolledText
+
+"""
+
+"""
+
+
+def load():
+    with open(filename.get()) as file:
+        contents.delete('1.0', END)
+        contents.insert(INSERT, file.read())
+
+
+def save():
+    with open(filename.get(), 'w') as file:
+        file.write(contents.get('1.0', END))
+
+
+def main():
+    top = Tk()
+    top.title("Simple Editor")
+    contents = ScrolledText()
+    contents.pack(side=BOTTOM, expand=True, fill=BOTH)
+    filename = Entry()
+    filename.pack(side=LEFT, expand=True, fill=X)
+    Button(text='Open', command=load).pack(side=LEFT)
+    Button(text='Save', command=save).pack(side=LEFT)
+    mainloop()
+
+
+if "__main__" == __name__:
+    main()
