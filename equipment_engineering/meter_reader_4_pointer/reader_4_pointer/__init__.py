357c980c13acf3b21721ff3cdab946bd4bb2028e
afterloe <605728727@qq.com>
2020-03-09 23:45:05 +0800

    feat: main.py函数优化

--- equipment_engineering/meter_reader_4_pointer/reader_4_pointer/__init__.py
+++ equipment_engineering/meter_reader_4_pointer/reader_4_pointer/__init__.py
@@ -1,23 +1,47 @@
 #!/usr/bin/env python3
 # -*- coding=utf-8 -*-
 
-import argparse
 from .argument_helper import ArgumentHelper
-from .logger import log
+from .logger import *
+import os
+
+ARGUMENT_HELPER = ArgumentHelper()
 
 
-if "__main__" == __name__:
+def version():
     log("指针式仪表识别软件 v1.2")
-    ap = argparse.ArgumentParser()
-    ap.add_argument("-d", "--debug", type=bool, help="开启可视化窗口， 进入debug模式", default=False)
-    ap.add_argument("-s", "--set", type=bool, help="进入设置模式", default=False)
-    ap.add_argument("-w", "--windows", type=bool, help="开启窗口模式", default=True)
-    args = vars(ap.parse_args())
-    setter = ArgumentHelper()
-    if True is args["set"]:
-        log("进入参数设置模式...")
-        min_angle = input("输入表盘最小值对应的刻度: ")
-        max_angle = input("输入表盘最大值对应的刻度: ")
-        min_value = input("输入表盘最小值: ")
-        max_value = input("输入表盘最大值: ")
-        setter.setArgument(min_angle, max_angle, min_value, max_value)
+
+
+def debug():
+    pass
+
+
+def input_number_check(key):
+    try:
+        value = input("输入%s: " % key)
+        if False is value.isnumeric():
+            assert Exception(key)
+        return int(value)
+    except:
+        log("%s必须为数字！" % key, ERROR)
+        os.exit(101)
+
+
+def set_detector_argument(min_angle, max_angle, min_value, max_value):
+    try:
+        if False is min_angle.isnumeric():
+            assert Exception("表盘最小值对应的刻度")
+        if False is max_angle.isnumeric():
+            assert Exception("表盘最大值对应的刻度")
+        if False is min_value.isnumeric():
+            assert Exception("表盘最小值")
+        if False is max_value.isnumeric():
+            assert Exception("表盘最大值")
+        min_angle = int(min_angle)
+        max_angle = int(max_angle)
+        min_value = int(min_value)
+        max_value = int(max_value)
+        ARGUMENT_HELPER.setArgument(min_angle, max_angle, min_value, max_value)
+    except Exception as e:
+        log("%s 必须为数字！" % e, ERROR)
+        assert Exception(e)
