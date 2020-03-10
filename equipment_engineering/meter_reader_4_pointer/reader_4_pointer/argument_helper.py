7901ba85455c8516b2622ad1df2a81867bcd10cb
afterloe <605728727@qq.com>
2020-03-10 15:48:07 +0800

    fix: 新版本无法读写config

--- equipment_engineering/meter_reader_4_pointer/reader_4_pointer/argument_helper.py
+++ equipment_engineering/meter_reader_4_pointer/reader_4_pointer/argument_helper.py
@@ -13,11 +13,13 @@ CHARACTER = "utf-8"
 class ArgumentHelper:
     _min_angle, _max_angle, _min_value, _max_value = 0, 0, 0, 0
     _util = ""
+    _bool = None
 
     def __init__(self):
         flag = os.path.isfile(DEFAULT_CONFIG_SAVE_PATH)
         if False is flag:
             log("%s 文件不存在，无法读取配置，按照默认配置进行" % DEFAULT_CONFIG_SAVE_PATH)
+            self._bool = True
             return
         with open(DEFAULT_CONFIG_SAVE_PATH, "r", encoding=CHARACTER) as f:
             config = yaml.load(f.read(), Loader=yaml.SafeLoader)
@@ -31,7 +33,7 @@ class ArgumentHelper:
             log("表盘最大值对应的刻度: %s" % self._max_angle)
             log("表盘最小值: %s" % self._min_value)
             log("表盘最大值: %s" % self._max_value)
-            log("单位: %s" % self.util)
+            log("单位: %s" % self._util)
 
     def setArgument(self, min_angle, max_angle, min_value, max_value, util):
         self._min_angle, self._max_angle = min_angle, max_angle
@@ -45,10 +47,11 @@ class ArgumentHelper:
             "util": self._util
         }
         config = None
-        with open(DEFAULT_CONFIG_SAVE_PATH, "r", encoding=CHARACTER) as f:
-            config = yaml.load(f.read(), Loader=yaml.SafeLoader)
-        if None is config:
+        if True is self._bool:
             config = {}
+        else:
+            with open(DEFAULT_CONFIG_SAVE_PATH, "r", encoding=CHARACTER) as f:
+                config = yaml.load(f.read(), Loader=yaml.SafeLoader)
         config["detector"] = detector_config
         with open(DEFAULT_CONFIG_SAVE_PATH, "w", encoding=CHARACTER) as f:
             yaml.dump(config, f, Dumper=yaml.SafeDumper)
@@ -57,7 +60,7 @@ class ArgumentHelper:
             log("表盘最大值对应的刻度: %s" % self._max_angle)
             log("表盘最小值: %s" % self._min_value)
             log("表盘最大值: %s" % self._max_value)
-            log("单位: %s" % self.util)
+            log("单位: %s" % self._util)
 
     def getArgument(self):
         return self._min_angle, self._max_angle, self._min_value, self._max_value, self._util
