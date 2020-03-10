e973275cac57f5d0b9a65eb0212b1932f7a64da9
afterloe <605728727@qq.com>
2020-03-10 15:38:13 +0800

    feat: v1.2.4发布，切换config配置

--- equipment_engineering/meter_reader_4_pointer/reader_4_pointer/argument_helper.py
+++ equipment_engineering/meter_reader_4_pointer/reader_4_pointer/argument_helper.py
@@ -11,10 +11,8 @@ CHARACTER = "utf-8"
 
 
 class ArgumentHelper:
-    _min_angle = 0
-    _max_angle = 0
-    _min_value = 0
-    _max_value = 0
+    _min_angle, _max_angle, _min_value, _max_value = 0, 0, 0, 0
+    _util = ""
 
     def __init__(self):
         flag = os.path.isfile(DEFAULT_CONFIG_SAVE_PATH)
@@ -23,36 +21,43 @@ class ArgumentHelper:
             return
         with open(DEFAULT_CONFIG_SAVE_PATH, "r", encoding=CHARACTER) as f:
             config = yaml.load(f.read(), Loader=yaml.SafeLoader)
-            self._min_angle = config["min_angle"]
-            self._max_angle = config["max_angle"]
-            self._min_value = config["min_value"]
-            self._max_value = config["max_value"]
+            detector_config = config["detector"]
+            self._min_angle = detector_config["min_angle"]
+            self._max_angle = detector_config["max_angle"]
+            self._min_value = detector_config["min_value"]
+            self._max_value = detector_config["util"]
             log("读取配置文件 %s" % DEFAULT_CONFIG_SAVE_PATH, SUCCESS)
             log("表盘最小值对应的刻度: %s" % self._min_angle)
             log("表盘最大值对应的刻度: %s" % self._max_angle)
             log("表盘最小值: %s" % self._min_value)
             log("表盘最大值: %s" % self._max_value)
+            log("单位: %s" % self.util)
 
-    def setArgument(self, min_angle, max_angle, min_value, max_value):
-        self._min_angle = min_angle
-        self._max_angle = max_angle
-        self._min_value = min_value
-        self._max_value = max_value
-        caps = {
+    def setArgument(self, min_angle, max_angle, min_value, max_value, util):
+        self._min_angle, self._max_angle = min_angle, max_angle
+        self._min_value, self._max_value = min_value, max_value
+        self._util = util
+        detector_config = {
             "min_angle": self._min_angle,
             "max_angle": self._max_angle,
             "min_value": self._min_value,
-            "max_value": self._max_value
+            "max_value": self._max_value,
+            "util": self._util
         }
-
+        config = None
+        with open(DEFAULT_CONFIG_SAVE_PATH, "r", encoding=CHARACTER) as f:
+            config = yaml.load(f.read(), Loader=yaml.SafeLoader)
+        if None is config:
+            config = {}
+        config["detector"] = detector_config
         with open(DEFAULT_CONFIG_SAVE_PATH, "w", encoding=CHARACTER) as f:
-            yaml.dump(caps, f, Dumper=yaml.SafeDumper)
+            yaml.dump(config, f, Dumper=yaml.SafeDumper)
             log("配置已写入 %s" % DEFAULT_CONFIG_SAVE_PATH, SUCCESS)
             log("表盘最小值对应的刻度: %s" % self._min_angle)
             log("表盘最大值对应的刻度: %s" % self._max_angle)
             log("表盘最小值: %s" % self._min_value)
             log("表盘最大值: %s" % self._max_value)
+            log("单位: %s" % self.util)
 
     def getArgument(self):
-        return self._min_angle, self._max_angle, self._min_value, self._max_value
-
+        return self._min_angle, self._max_angle, self._min_value, self._max_value, self._util
