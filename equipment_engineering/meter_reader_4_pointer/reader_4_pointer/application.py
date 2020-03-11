66d94cbc3ca79fd93917f2382f328255abc92194
afterloe <605728727@qq.com>
2020-03-11 16:44:18 +0800

    feat: 基于帧差法的表盘识别

--- equipment_engineering/meter_reader_4_pointer/reader_4_pointer/application.py
+++ equipment_engineering/meter_reader_4_pointer/reader_4_pointer/application.py
@@ -5,7 +5,7 @@ import cv2 as cv
 from imutils.video import FPS
 from imutils.video import VideoStream
 from .logger import log, ERROR
-from .process_util import draw_box, infer_diff
+from .process_util import draw_box, infer_diff, meter_detection, avg_circles, infer
 import time
 
 
@@ -27,11 +27,24 @@ class Application:
             frame_with_box, (x, y, w, h) = draw_box(frame)
             roi_next = frame[y: h, x: w, :]
             flag, previous = infer_diff(self._roi_previous, roi_next)
+            if vision:
+                cv.imshow("watch dog", frame_with_box)
             if False is flag:
                 continue
             self._roi_previous = previous
+            flag, meter = meter_detection(roi_next)
+            if False is flag:
+                continue
+            flag, pointer = pointer_detection(roi_next)
+            if False is flag:
+                continue
+            a, b, c = meter.shape
+            x, y, _ = avg_circles(meter, b)
+            value = infer(x, y, pointer, self._min_angle, self._max_angle, self._min_value, self._max_value)
+            log("value is {:.3f} {}".format(value, self._util))
             if vision:
-                cv.imshow("watch dog", frame_with_box)
+                cv.putText(frame_with_roi, "{:.3f} {}".format(value, self._util), (w, h),
+                           cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2, cv.LINE_AA)
             key = cv.waitKey(100) & 0xff
             self.process_with_key(key, vision)
 
