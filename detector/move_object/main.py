335cba0b4da9675150601f8960aaeff54b59e3c7
afterloe <605728727@qq.com>
2020-03-11 22:01:00 +0800

    feat: 帧差法过滤

--- detector/move_object/main.py
+++ detector/move_object/main.py
@@ -2,37 +2,53 @@
 # -*- coding=utf-8 -*-
 
 import cv2 as cv
+import numpy as np
+import imutils
 from imutils.video import VideoStream
 
 """
 
 """
 
+hsv_min, hsv_max = (0, 0, 0), (180, 255, 50)
+kernel = cv.getStructuringElement(cv.MORPH_RECT, (3, 3))
+morphology_param = dict(op=cv.MORPH_OPEN, kernel=kernel)
+
+
+def process(image):
+    hsv = cv.cvtColor(image.copy(), cv.COLOR_BGR2HSV)
+    mask = cv.inRange(hsv, hsv_min, hsv_max)
+    blurred = cv.GaussianBlur(mask, (0, 0), 3)
+    return blurred
+    # cv.imshow("bin", blurred)
+
 
 def main():
     vs = VideoStream(src=0).start()
-    padding, color = 300, (0, 0, 255)
-    hsv_min, hsv_max = (0, 0, 0), (180, 255, 50)
-    image = vs.read()
-    h, w = image.shape[: 2]
-    x, y, width, height = w // 2 - padding // 2, h // 2 - padding // 2, padding, padding
-    # cv.rectangle(image, (x, y), (x + width, y + height), color, 2, cv.LINE_AA)
-    roi = image[y: y + height, x: x + width, :]
-    hsv = cv.cvtColor(roi.copy(), cv.COLOR_BGR2HSV)
-    mask = cv.inRange(hsv, hsv_min, hsv_max)
-    edged = cv.GaussianBlur(mask, (0, 0), 3)
-    hist = cv.calcHist([hsv], [0], edged, [180], [0, 180])
-    cv.normalize(hist, hist, 0, 255, cv.NORM_MINMAX)
+    pre_1 = process(vs.read())
+    pre_2 = process(vs.read())
+
     while True:
         frame = vs.read()
-        roi = frame[y: y + height, x: x + width, :]
-        hsv = cv.cvtColor(roi, cv.COLOR_BGR2HSV)
-        dst = cv.calcBackProject([hsv], [0], hist, [0, 180], 1)
-        track_box = cv.CamShift(dst, (x, y, width, height), (cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 1))
-        print(len(track_box))
-        # cv.ellipse(frame, track_box[0], (0, 0, 255), 3, cv.LINE_AA)
-        # cv.imshow("watch dog", frame)
-        key = cv.waitKey(0) & 0xff
+        now = process(frame)
+        diff_1 = cv.subtract(pre_2, pre_1)
+        diff_2 = cv.subtract(now, pre_1)
+        diff = cv.bitwise_and(diff_1, diff_2)
+        binary = cv.morphologyEx(diff, **morphology_param)
+        contours = cv.findContours(binary, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+        contours = imutils.grab_contours(contours)
+        for cnt in contours:
+            x, y, w, h = cv.boundingRect(cnt)
+            if 100 > h and 100 > w:
+                continue
+            cv.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+        pre_1 = np.copy(pre_2)
+        pre_2 = np.copy(now)
+        cv.imshow("watch dog", frame)
+        if None is not diff:
+            cv.imshow("dst", diff)
+        key = cv.waitKey(100) & 0xff
         if ord("q") == key:
             break
     vs.stop()
