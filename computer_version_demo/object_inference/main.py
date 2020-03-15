84e3dd85766963f8793b32fba98a78cf45c7b439
afterloe <605728727@qq.com>
2020-03-15 17:34:56 +0800

    feat: 对象检测课程

---
+++ computer_version_demo/object_inference/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding=utf-8 -*-
+
+import argparse
+from .sample import FaceDetection
+
+if "__main__" == __name__:
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--image", required=True, help="检测图片地址")
+    ap.add_argument("-p", "--prototxt", required=True, help="Caffe模型描述文件")
+    ap.add_argument("-m", "--model", required=True, help="Caffe模型文件")
+    ap.add_argument("-c", "--confidence", type=float, default=0.5, help="检测阈值")
+    args = vars(ap)
+    detector = FaceDetection(args["model"], args["prototxt"])
+    detector.setConfidence(args["confidence"])
+    detector.loadImage(args["image"])
