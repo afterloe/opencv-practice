087bd758212f78781598782f6b439c94aad8f3b1
afterloe <605728727@qq.com>
2020-03-17 14:16:16 +0800

    图形追踪

---
+++ computer_version_demo/object_tract/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding=utf-8 -*-
+
+import argparse
+from sample.object_tract import ObjectTract
+
+if "__main__" == __name__:
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-v", "--video", required=True, help="输入检测图像")
+    ap.add_argument("-b", "--buffer", type=int, default=64, help="最大缓冲区大小")
+    args = vars(ap.parse_args())
+    tractUtil = ObjectTract(video=ap["video"], max_buff=ap["buffer"])
+    tractUtil.start_track()
