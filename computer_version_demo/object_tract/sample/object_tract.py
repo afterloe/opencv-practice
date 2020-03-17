8fa5f398c3cef3f28dc41b73c417fb6b05a53681
afterloe <605728727@qq.com>
2020-03-17 14:36:45 +0800

    fix: videoStream无法读取mp4的异常

--- computer_version_demo/object_tract/sample/object_tract.py
+++ computer_version_demo/object_tract/sample/object_tract.py
@@ -12,20 +12,29 @@ class ObjectTract(object):
     __fps, __loop, __key = None, True, None
     __yellow_lower, __yellow_upper = (26, 43, 46), (34, 255, 255)
 
-    def __init__(self, source=0, max_buff=64):
+    def __init__(self, video, max_buff=64):
         self.__max_buf = max_buff
-        self.__vs = VideoStream(src=source)
+        if not video:
+            self.__vs = VideoStream(src=0).start()
+        else:
+            self.__vs = cv.VideoCapture(video)
+        self.__video = video
 
     def __del__(self):
-        self.__vs.stop()
         cv.destroyAllWindows()
+        if not self.__video:
+            self.__vs.stop()
+        else:
+            self.__vs.release()
 
     def start_track(self):
-        self.__vs.start()
+        print("begin to track")
         time.sleep(1.0)
         while self.__loop:
             frame = self.__vs.read()
+            frame = frame[1] if self.__video else frame
             if None is frame:
+                print("video can't read")
                 break
             frame = imutils.resize(frame, width=600)
             blurred = cv.GaussianBlur(frame, (11, 11), 0)
