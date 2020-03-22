74c0280338fade36e871576d14a8c59bb5ca88a0
afterloe <605728727@qq.com>
2020-03-22 15:10:34 +0800

    feat: 模型训练

--- computer_version_demo/deep_learning/sample/train_util.py
+++ computer_version_demo/deep_learning/sample/train_util.py
@@ -20,14 +20,31 @@ import os
 
 CONSOLE = logging.getLogger("dev")
 
+EPOCHS = 25
+INIT_LR = 1e-3
+BS = 32
+
 
 class TrainLeNet(object):
 
-    def __init__(self):
+    def __init__(self, path_of_images):
+        self.__data = []
+        self.__labels = []
+        self.__path_of_images = sorted(list(paths.list_images(path_of_images)))
+        random.seed(42)
+        random.shuffle(path_of_images)
         pass
 
     def __del__(self):
         pass
 
     def run(self):
+        for image_path in self.__path_of_images:
+            image = cv.imread(image_path)
+            image = cv.resize(image, (28, 28))
+            image = img_to_array(image)
+            self.__data.append(image)
+            label = image_path.split(os.path.sep)[-2]
+            label = 1 if label == "nazha" else 0
+            self.__labels.append(label)
         pass
