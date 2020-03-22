7b73cbed703c1636c806afd34bb8bb737b326885
afterloe <605728727@qq.com>
2020-03-22 18:18:25 +0800

    feat: 图像训练器

--- computer_version_demo/deep_learning/sample/train_util.py
+++ computer_version_demo/deep_learning/sample/train_util.py
@@ -27,6 +27,8 @@ BS = 32
 
 class TrainLeNet(object):
 
+    __out_dir, __H = None, None
+
     def __init__(self, path_of_images):
         self.__data = []
         self.__labels = []
@@ -38,7 +40,16 @@ class TrainLeNet(object):
     def __del__(self):
         pass
 
+    @property
+    def outs(self):
+        return self.__out_dir
+
+    @outs.setter
+    def outs(self, out_dir):
+        self.__out_dir = out_dir
+
     def run(self):
+        # pre process
         for image_path in self.__path_of_images:
             image = cv.imread(image_path)
             image = cv.resize(image, (28, 28))
@@ -47,4 +58,34 @@ class TrainLeNet(object):
             label = image_path.split(os.path.sep)[-2]
             label = 1 if label == "nazha" else 0
             self.__labels.append(label)
-        pass
+        self.__data = np.array(self.__data, dtype="float") / 255.0
+        train_x, test_x, train_y, test_y = train_test_split(self.__data, self.__labels, test_size=0.25, random_state=42)
+        train_y = to_categorical(train_y, num_classes=2)
+        test_y = to_categorical(test_y, num_classes=2)
+
+        aug = ImageDataGenerator(rotation_range=30, width_shift_range=0.1, height_shift_range=0.1,
+                                 shear_range=0.2, zoom_range=0.2, horizontal_flip=True, fill_mode="nearest")
+
+        CONSOLE.info("编译模型")
+        model = LeNet.build(width=28, height=28, channel=3, classes=2)
+        opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
+        model.compile(loss="binary_crossentropy", optimizer=opt, metrics=["accuracy"])
+        CONSOLE.info("训练模型")
+        self.__H = model.fit_generator(aug.flow(train_x, train_y, batch_size=BS), validation_data=(test_x, test_y),
+                                steps_per_epoch=len(train_x) // BS, epochs=EPOCHS, verbose=1)
+        CONSOLE.info("序列化模型")
+        model.save(self.__out_dir)
+
+    def draw_plt(self, path_of_plt):
+        plt.style.use("ggplot")
+        plt.figure()
+        N = EPOCHS
+        plt.plot(np.arange(0, N), self.__H.history["loss"], label="train_loss")
+        plt.plot(np.arange(0, N), self.__H.history["val_loss"], label="val_loss")
+        plt.plot(np.arange(0, N), self.__H.history["acc"], label="train_acc")
+        plt.plot(np.arange(0, N), self.__H.history["val_acc"], label="val_acc")
+        plt.title("Training Loss and Accuracy on Santa/Not Santa")
+        plt.xlabel("Epoch #")
+        plt.ylabel("Loss/Accuracy")
+        plt.legend(loc="lower left")
+        plt.savefig(path_of_plt)
