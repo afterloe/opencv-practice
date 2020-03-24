17bb4ae1cc16208b59d433939bfab880e478d7a1
afterloe <605728727@qq.com>
2020-03-24 20:33:30 +0800

    feat: CNNs模型构建

---
+++ computer_version_demo/cnns_nerual/smallervggnet.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding=utf-8 -*-
+
+
+from keras.models import Sequential
+from keras.layers.normalization import BatchNormalization
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.layers.core import Activation, Flatten, Dropout, Dense
+from keras import backend as K
+
+
+class SmallerVGGNet(object):
+
+    @staticmethod
+    def build(width, height, channel, classes):
+        model = Sequential()
+        input_shape = (height, width, channel)
+        chan_dim = -1
+        if "channels_first" == K.image_data_format():
+            input_shape = (channel, height, width)
+            chan_dim = 1
+
+        # CONV => RELU => POOL
+        model.add(Conv2D(32, (3, 3), padding="same", input_shape=input_shape))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chan_dim))
+        model.add(MaxPooling2D(pool_size=(3, 3)))
+        model.add(Dropout(0.25))
+
+        # (CONV => RELU) * 2 => POOL
+        model.add(Conv2D(64, (3, 3), padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chan_dim))
+        model.add(Conv2D(64, (3, 3), padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chan_dim))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+
+        # another (CONV => RELU) * 2 => POOL
+        model.add(Conv2D(128, (3, 3), padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chan_dim))
+        model.add(Conv2D(128, (3, 3), padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chan_dim))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+
+        # FC => RELU
+        model.add(Flatten())
+        model.add(Dense(1024))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.5))
+        model.add(Dense(classes))
+        model.add(Activation("softmax"))
+
+        return model
