16b6b3bf0fffacc3ec8f10d9795c948d8ade5a6d
afterloe <605728727@qq.com>
2020-03-28 15:53:27 +0800

    docs: 添加了knn训练的信息

--- deep_learning/first_image_classifier/knn.py
+++ deep_learning/first_image_classifier/knn.py
@@ -32,11 +32,19 @@ if "__main__" == __name__:
     data_loader = SimpleDatasetLoader(preprocessor=[processor])
     data, labels = data_loader.load(image_paths, verbose=500)
     data = data.reshape((data.shape[0], 3072))
+    # 32×32×3 的特征点， 878张图片共计 2.6MB的内存占用
     CONSOLE.info("特征点: {:.1f}MB".format(data.nbytes / (1024 * 1000.0)))
+    #  将标签(字符串) 转换为整数（每个类的唯一整数）
+    #  eg：这种转换允许将cat类映射到整数0
     le = LabelEncoder()
     labels = le.fit_transform(labels)
+    # 将训练和测试拆分，75%的数据用于训练，25%用于测试。
     train_x, test_x, train_y, test_y = train_test_split(data, labels, test_size=0.25, random_state=42)
     CONSOLE.info("knn 分类")
+    # 初始化 KNeighborsClassifier类
     model = KNeighborsClassifier(n_neighbors=args["neighbors"], n_jobs=args["jobs"])
+    # 训练
     model.fit(train_x, train_y)
-    CONSOLE.info(classification_report(test_y, model.predict(test_x), target_names=le.classes_))
+    CONSOLE.info("结果如下:")
+    # 分类报告函数来评估分类器。需要提供 testY 类标签，以及可选的类别标签的名称(即 转换后标签属性)。
+    print(classification_report(test_y, model.predict(test_x), target_names=le.classes_))
