bfff1536eab6d371616c78dc7b69224e0212bc94
afterloe <605728727@qq.com>
2020-04-18 00:45:32 +0800

    fix: 图像转dataset出现的异常

--- tf_aip_workshops/3-section/img_to_dataset.py
+++ tf_aip_workshops/3-section/img_to_dataset.py
@@ -12,7 +12,7 @@ import logging
 
 slim = tf.contrib.slim
 __version__ = "1.0.0"
-image = nasnet.build_nasnet_mobile.defautlt_image_size
+image = nasnet.build_nasnet_mobile.default_image_size  # 224
 
 logging.basicConfig(level=logging.INFO,
                     format='[%(asctime)8s][%(filename)s][%(levelname)s] - %(message)s',
@@ -28,7 +28,8 @@ def get_images_list(directory):
     files_and_labels = []
     for label in labels:
         images = list_images(os.path.sep.join([directory, label]))
-        files_and_labels.append((*images, label))
+        for i in images:
+            files_and_labels.append((i, label))
     file_names, labels = zip(*files_and_labels)
     file_names = list(file_names)
     labels = list(labels)
@@ -36,17 +37,12 @@ def get_images_list(directory):
     label_to_int = {}
     for i, label in enumerate(sorted(unique_labels)):
         label_to_int[label] = i + 1
-        CONSOLE.info(label, label_to_int[label])
     labels = [label_to_int[l] for l in labels]
-    CONSOLE.info(labels[:6])
-    CONSOLE.info(labels[-6:])
     return file_names, labels
 
 
 if "__main__" == __name__:
     ap = argparse.ArgumentParser()
-    ap.add_argument("-d", "--dir", required=True, help="图像目录", type=str)
-    args = vars(ap)
-    fn, l = get_images_list(args["dir"])
-    CONSOLE.info(fn)
-    CONSOLE.info(l)
+    ap.add_argument("-d", "--dataset", required=True, help="图像目录", type=str)
+    args = vars(ap.parse_args())
+    fn, l = get_images_list(args["dataset"])
