7e2f36e1fab985e189de563c42dc7aaf8b5daba4
afterloe <605728727@qq.com>
2020-04-18 23:59:31 +0800

    feat: 测试脚本

---
+++ tf_aip_workshops/3-section/test.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+# -*- coding=utf-8 -*-
+
+import argparse
+import numpy as np
+from PIL import Image
+import tensorflow as tf
+from nets.nasnet import nasnet
+from matplotlib import pyplot as plt
+import logging
+import os
+
+model_clazz = __import__("model")
+slim = tf.contrib.slim
+
+image_size = nasnet.build_nasnet_mobile.default_image_size  # 224
+
+
+def check_accuracy(sess):
+    sess.run(model.test_init_op)
+    num_correct, num_samples = 0, 0
+    i = 0
+    while True:
+        i += 1
+        CONSOLE.info(i)
+        try:
+            correct_prediction, accuracy, logits = sess.run([model.correct_prediction, model.accuracy, model.logits])
+            num_correct += correct_prediction.sum()
+            num_samples += correct_prediction.shape[0]
+            CONSOLE.info("accuracy {} {}".format(accuracy, logits))
+        except tf.errors.OutOfRangeError:
+            CONSOLE.info("over")
+            break
+        acc = float(num_correct) / num_samples
+        return acc
+
+
+def check_digital(image_dir, sess):
+    image = Image.open(image_dir)
+    if "RGB" != image.mode:
+        image = image.convert("RGB")
+    image = np.asarray(image.resize((image_size, image_size)), dtype=np.float32).reshape(1, image_size, image_size, 3)
+    image = 2 * (image / 255.0) - 1.0
+    prediction = sess.run(model.logits, {model.images: image})
+    CONSOLE.info(prediction)
+    pre = prediction.argmax()
+    CONSOLE.info(pre)
+    content = ""
+    if 0 == pre:
+        content = "cat"
+    elif 1 == pre:
+        content = "dog"
+    elif 2 == pre:
+        content = "panda"
+    plt.imshow(np.asarray((image[0] + 1) * 255 / 2, np.uint8))
+    plt.show()
+    CONSOLE.info("%s -- %s" % (content, image_dir))
+    return pre
+
+
+if "__main__" == __name__:
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-d", "--dir", required=True, help="测试图像路径", type=str)
+    ap.add_argument("-b", "--batch-size", default=32, type=int, help="批处理次数")
+    args = vars(ap.parse_args())
+    logging.basicConfig(level=logging.INFO,
+                        format='[%(asctime)8s][%(filename)s][%(levelname)s] - %(message)s',
+                        datefmt='%a, %d %b %Y %H:%M:%S')
+    CONSOLE = logging.getLogger("dev")
+    model = model_clazz.CustomizeNASNetModel()
+    model.build_model("test", args["dir"])
+    with tf.Session() as session:
+        model.load_cpk(model.global_step, session, 1, model.saver, model.save_path)
+        val_acc = check_accuracy(session)
+        CONSOLE.info("val accuracy: %f" % val_acc)
+        img_dir = "/home/afterloe resources/12485.jpg"
+        check_digital(img_dir, session)
+        img_dir = os.path.sep.join([args["dir"], "cat", "cat.27.jpg"])
+        check_digital(img_dir, session)
+        img_dir = os.path.sep.join([args["dir"], "dog", "dog.93.jpg"])
+        check_digital(img_dir, session)
