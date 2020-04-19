076425c267aad18e99edeaabd559ff30bf86bb45
afterloe <605728727@qq.com>
2020-04-19 18:12:23 +0800

    refactor: 调整训练代码

--- tf_aip_workshops/3-section/train.py
+++ tf_aip_workshops/3-section/train.py
@@ -18,45 +18,42 @@ def main(model, batch_size, train_dir, eval_dir):
 
     with tf.Session() as sess:
         sess.run(mode.global_init)
+        step = mode.load_cpk(mode.global_step, sess, 1, mode.saver, mode.save_path)
+        CONSOLE.info(step)
+        if 0 == step:
+            mode.init_fn(sess)
+            for epoch in range(num_epochs_1):
+                CONSOLE.info("Staring_1 epoch %d / %d" % (epoch + 1, num_epochs_1))
+                sess.run(mode.train_init_op)
+                while True:
+                    try:
+                        step += 1
+                        acc, accuracy_top_5, summary, _ = sess.run([mode.accuracy, mode.accuracy_top_5, mode.merged,
+                                                                    mode.last_train_op])
+                        if 0 == step % 100:
+                            CONSOLE.info(f"step: {step} train_1 accuracy: {acc}, {accuracy_top_5}")
+                    except tf.errors.OutOfRangeError:
+                        CONSOLE.info("train_1: %d OK" % epoch)
+                        mode.saver.save(sess, os.path.sep.join([mode.save_path, "digital_nasnet.cpkt"]),
+                                        global_step=mode.global_step.eval())
+                        break
+                sess.run(mode.step_init)
 
-    step = 0
-    step = mode.load_cpk(mode.global_step, sess, 1, mode.saver, mode.save_path)
-    CONSOLE.info(step)
-
-    if 0 == step:
-        mode.init_fn(sess)
-        for epoch in range(num_epochs_1):
-            CONSOLE.info("Staring_1 epoch %d / %d" % (epoch + 1, num_epochs_1))
-            sess.run(mode.train_init_op)
-            while True:
-                try:
-                    step += 1
-                    acc, accuracy_top_5, summary, _ = sess.run([mode.accuracy, mode.accuracy_top_5, mode.merged,
-                                                                mode.last_train_op])
-                    if 0 == step % 100:
-                        CONSOLE.info(f"step: {step} train_1 accuracy: {acc}, {accuracy_top_5}")
-                except tf.errors.OutOfRangeError:
-                    CONSOLE.info("train_1: %d OK" % epoch)
-                    mode.saver.save(sess, os.path.sep.join([mode.save_path, "digital_nasnet.cpkt"]),
-                                    global_step=mode.global_step.eval())
-                    break
-            sess.run(mode.step_init)
-
-        for epoch in range(num_epochs_2):
-            CONSOLE.info("Staring_2 epoch %d / %d" % (epoch + 1, num_epochs_2))
-            sess.run(mode.train_init_op)
-            while True:
-                try:
-                    step += 1
-                    acc, summary, _ = sess.run([mode.accuracy, mode.merged, mode.full_train_op])
-                    mode.train_writer.add_summary(summary, step)
-                    if 0 == step % 100:
-                        CONSOLE.info(f"step: {step} train_2 accuracy: {acc}")
-                except tf.errors.OutOfRangeError:
-                    CONSOLE.info("train_2 %d OK" % epoch)
-                    mode.saver.save(sess, os.path.sep.join([mode.save_path, "digital_nasnet.cpkt"]),
-                                    global_step=mode.global_step.eval())
-                    break
+            for epoch in range(num_epochs_2):
+                CONSOLE.info("Staring_2 epoch %d / %d" % (epoch + 1, num_epochs_2))
+                sess.run(mode.train_init_op)
+                while True:
+                    try:
+                        step += 1
+                        acc, summary, _ = sess.run([mode.accuracy, mode.merged, mode.full_train_op])
+                        mode.train_writer.add_summary(summary, step)
+                        if 0 == step % 100:
+                            CONSOLE.info(f"step: {step} train_2 accuracy: {acc}")
+                    except tf.errors.OutOfRangeError:
+                        CONSOLE.info("train_2 %d OK" % epoch)
+                        mode.saver.save(sess, os.path.sep.join([mode.save_path, "digital_nasnet.cpkt"]),
+                                        global_step=mode.global_step.eval())
+                        break
 
 
 if "__main__" == __name__:
